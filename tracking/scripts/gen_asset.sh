dbfe7ef1cf13fe63389e3702fb1dc9d8f46da67c
Anhltn1609 <anhsltn1234r@gmail.com>
2025-02-28 11:38:23 +0700

    Remove unwanted tracked files

--- tracking/scripts/gen_asset.sh
+++
@@ -1,4 +0,0 @@
-cd assets
-fluttergen
-cd ..
-fvm flutter pub global run intl_utils:generate
--- tracking/scripts/pub_get_all.sh
+++
@@ -1,7 +0,0 @@
-flutter pub get
-
-cd ../maps_plugin
-flutter pub get
-
-cd ../tracking_core
-flutter pub get
